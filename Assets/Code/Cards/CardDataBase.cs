e69e02efdf63b149b54e72baa53be5062c71f7b9
Patpig10 <99894564+Patpig10@users.noreply.github.com>
2024-01-29 14:18:39 +0000

    Feild

--- Assets/Code/Cards/CardDataBase.cs
+++ Assets/Code/Cards/CardDataBase.cs
@@ -11,7 +11,7 @@ public class CardDataBase : MonoBehaviour
         cardList.Add(new Card (0, "None", 0, 0, "None", Resources.Load <Sprite>("1"),"None", 0,0,0,0,0));
         cardList.Add(new Card(1, "Ezeh", 2, 3000, "Warrior of light", Resources.Load < Sprite>("2"), "White",0,0,0,0,0));
         cardList.Add(new Card(2, "Luci", 1, 5000, "Cute dummy", Resources.Load < Sprite>("3"),"Blue", 0,0,0,0,0));
-        cardList.Add(new Card(3, "Liz", 3, 7000, "power boosrt, Draw 1", Resources.Load < Sprite>("4"), "Green", 1, 0,0,0,200000));
+        cardList.Add(new Card(3, "Liz", 3, 7000, "power boosrt 500, Draw 1", Resources.Load < Sprite>("4"), "Green", 1, 0,0,0,500));
         cardList.Add(new Card(4, "Kris", 2, 40000, "From Grave To hand, 2 Gil", Resources.Load < Sprite>("5"), "Black", 0, 2,1,500000,0));
 
     }
