b45c6940068564fbbaca80033c25387bd2d62489
Patpig10 <99894564+Patpig10@users.noreply.github.com>
2024-02-10 21:18:08 +0000

    Collection scene (No code)

--- Assets/Code/Cards/CardDataBase.cs
+++ Assets/Code/Cards/CardDataBase.cs
@@ -8,12 +8,14 @@ public class CardDataBase : MonoBehaviour
 
     void Awake()
     {
+        //monster
         cardList.Add(new Card (0, "None", 0, 0, "None", Resources.Load <Sprite>("1"),"None", 0,0,0,0,0,false, 0));
-        cardList.Add(new Card(1, "Ezeh", 2, 3000, "Warrior of light", Resources.Load < Sprite>("2"), "White",0,0,0,0,0, false, 0));
-        cardList.Add(new Card(2, "Luci", 1, 5000, "Cute dummy", Resources.Load < Sprite>("3"),"Blue", 0,0,0,0,0, false, 0));
-        cardList.Add(new Card(3, "Liz", 3, 7000, "power boosrt 500, Draw 1", Resources.Load < Sprite>("4"), "Green", 1, 0,0,0,500,false,0));
-        cardList.Add(new Card(4, "Kris", 2, 40000, "From Grave To hand, 2 Gil", Resources.Load < Sprite>("5"), "Black", 0, 2,1,500000,0, false, 0));
-        cardList.Add(new Card(5, "Heal", 2, 0, "Spell Card damage 3000", Resources.Load<Sprite>("Heal"), "Blue", 0, 0, 0, 0, 0, true, 3000));
+        cardList.Add(new Card(1, "Ezeh", 2, 30, "Warrior of light", Resources.Load < Sprite>("2"), "White",0,0,0,0,0, false, 0));
+        cardList.Add(new Card(2, "Luci", 1, 50, "Cute dummy", Resources.Load < Sprite>("3"),"Blue", 0,0,0,0,0, false, 0));
+        cardList.Add(new Card(3, "Liz", 3, 70, "power boosrt 500, Draw 1", Resources.Load < Sprite>("4"), "Green", 1, 0,0,0,500,false,0));
+        cardList.Add(new Card(4, "Kris", 2, 400, "From Grave To hand, 2 Gil", Resources.Load < Sprite>("5"), "Black", 0, 2,1,500000,0, false, 0));
+        //spells
+        cardList.Add(new Card(5, "Holy Light", 1, 0, "Spell Card damage 3000", Resources.Load<Sprite>("Heal"), "Blue", 0, 0, 0, 0, 0, true, 3000));
 
     }
 }
